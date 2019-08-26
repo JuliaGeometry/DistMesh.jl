ea2ca4ccb423dde98a7ae90d839d691e5d285867
steve <kd2cca@gmail.com>
2019-08-25 22:28:49 -0400

    add matlab style unique compatibility function

--- src/DistMesh.jl
+++ src/DistMesh.jl
@@ -3,12 +3,11 @@ module DistMesh
 using GeometryTypes,
       Meshing,
       StaticArrays
-include("GroupSlices.jl")
-using .GroupSlices
 
 #include("circumcenter.jl")
 #include("distmeshsurface.jl")
 include("dsphere.jl")
 include("mkt2t.jl")
+include("munique.jl")
 
 end # module
