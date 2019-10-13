131f9875699f2cf6010bc17d34bb848b6b5efd4b
steve <kd2cca@gmail.com>
2019-10-13 19:45:29 -0400

    specify mat file location

--- test/mkt2t.jl
+++ test/mkt2t.jl
@@ -4,7 +4,7 @@
 @testset "mkt2t" begin
     # load up matlab workspace for comparison
     # based on sphere case
-    vars = matread("mats/mkt2t.mat")
+    vars = matread((@__DIR__)*"/mats/mkt2t.mat")
     test_t2t, test_t2n = DistMesh.mkt2t(vars["t"])
     @test test_t2n == vars["t2n"]
     @test test_t2t == vars["t2t"]
