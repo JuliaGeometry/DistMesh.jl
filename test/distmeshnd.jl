c9f6f243a5961d49bb8f68b7ece2b6f0e4f7786c
steve <kd2cca@gmail.com>
2019-10-20 19:21:59 -0400

    add test for sphere

--- test/distmeshnd.jl
+++ test/distmeshnd.jl
@@ -1,8 +1,7 @@
-dim=3
-d(p) = sqrt(sum(p.^2))-1
-p,t = distmeshnd(d,huniform,0.2)
 
-
-#fi = vtk_grid("tet",[x[1] for x in p],[x[2] for x in p],[x[3] for x in p],map(x->MeshCell(10, [x...]),t))
-#vtk_cell_data(fi, )
-#vtk_save(fi)
+@testset begin
+    d(p) = sqrt(sum(p.^2))-1
+    p,t = distmeshnd(d,huniform,0.2)
+    @test length(p) == 485
+    @test length(t) == 2057
+end
