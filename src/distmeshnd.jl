3a4069520902663a36d851dba945903b07f45a8c
steve <kd2cca@gmail.com>
2019-10-24 22:36:24 -0400

    add docstring

--- src/distmeshnd.jl
+++ src/distmeshnd.jl
@@ -1,28 +1,25 @@
-function distmeshnd(fdist,fh,h, ::Type{VertType}=GeometryBasics.Point{3,Float64}; origin=VertType(-1,-1,-1),
+"""
+    distmesh
+    3D Mesh Generator using Signed Distance Functions.
+    Arguments:
+        fdist:       Distance function
+        fh:          Edge length function
+        h:           Smallest edge length
+
+    Returns:
+        p:           Node positions
+        t:           Triangle indices
+
+
+    Example: Unit ball
+        d(p) = sqrt(sum(p.^2))-1
+        p,t = distmeshnd(d,huniform,0.2)
+"""
+function distmeshnd(fdist::Function,fh::Function,h::Number, ::Type{VertType}=GeometryBasics.Point{3,Float64}; origin=VertType(-1,-1,-1),
                                                                        widths=VertType(2,2,2),
                                                                        fix::Vector{VertType}=VertType[],
                                                                        vis=true,
                                                                        distribution=:regular) where {VertType}
-    # %DISTMESHND N-D Mesh Generator using Distance Functions.
-    # %   [P,T]=DISTMESHND(FDIST,FH,H,BOX,FIX,FDISTPARAMS)
-    # %
-    # %      P:           Node positions (NxNDIM)
-    # %      T:           Triangle indices (NTx(NDIM+1))
-    # %      FDIST:       Distance function
-    # %      FH:          Edge length function
-    # %      H:           Smallest edge length
-    # %      BOX:         Bounding box [xmin,xmax;ymin,ymax; ...] (NDIMx2)
-    # %      FIX:         Fixed node positions (NFIXxNDIM)
-    # %      FDISTPARAMS: Additional parameters passed to FDIST
-    # %
-    # %   Example: Unit ball
-    # %      dim=3;
-    # %      d=inline('sqrt(sum(p.^2,2))-1','p');
-    # %      [p,t]=distmeshnd(d,@huniform,0.2,[-ones(1,dim);ones(1,dim)],[]);
-    # %
-    # %   See also: DISTMESH2D, DELAUNAYN, TRIMESH, MESHDEMOND.
-
-    # %   Copyright (C) 2004-2012 Per-Olof Persson. See COPYRIGHT.TXT for details.
 
     dim=length(VertType)
     ptol=.001; ttol=0.02; L0mult=1+.4/2^(dim-1); deltat=0.05; geps=1e-1*h;
