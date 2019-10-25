874b6a2dffe40e8713cb0feb0a44515d53d7bc48
steve <kd2cca@gmail.com>
2019-10-24 23:01:45 -0400

    collapse tests into one folder

--- test/munique.jl
+++
@@ -1,7 +0,0 @@
-
-
-
-@testset "munique" begin
-    a = [1 2; 3 4; 5 6; 1 2; 3 4; 8 8]
-    @test DistMesh.munique(a) == [1,2,3,1,2,4]
-end
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,38 @@ using MAT
 # use makie to visualize triangulations
 _VIS = false
 
-include("distmeshnd.jl")
+
+@testset "distmesh 3D" begin
+    d(p) = sqrt(sum(p.^2))-1
+    p,t = distmesh(d,huniform,0.2, vis=_VIS)
+    @test length(p) == 485
+    @test length(t) == 2207
+
+    p,t = distmesh(d,huniform,0.2, vis=_VIS, distribution=:packed)
+    @test length(p) == 742
+    @test length(t) == 3455
+end
+
 
 # translation utils
-include("munique.jl")
+@testset "munique" begin
+    a = [1 2; 3 4; 5 6; 1 2; 3 4; 8 8]
+    @test DistMesh.munique(a) == [1,2,3,1,2,4]
+end
 
 # distmesh utils
-include("mkt2t.jl")
-include("trisurfupd.jl")
+@testset "mkt2t" begin
+    # load up matlab workspace for comparison
+    # based on sphere case
+    vars = matread((@__DIR__)*"/mats/mkt2t.mat")
+    test_t2t, test_t2n = DistMesh.mkt2t(vars["t"])
+    @test test_t2n == vars["t2n"]
+    @test test_t2t == vars["t2t"]
+
+    # close?
+end
+
+
 
 # @testset "distmeshsurface" begin
 #     fd(p) = dsphere(p,0,0,0,1);
