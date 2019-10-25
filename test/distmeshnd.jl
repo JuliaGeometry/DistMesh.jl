ac0a7414c1ab031c83a851d82b2f150bbda4f33e
steve <kd2cca@gmail.com>
2019-10-24 22:59:05 -0400

    add additional test for distmesh

--- test/distmeshnd.jl
+++ test/distmeshnd.jl
@@ -1,7 +1,10 @@
 
-@testset begin
+@testset "distmesh 3D" begin
     d(p) = sqrt(sum(p.^2))-1
     p,t = distmesh(d,huniform,0.2, vis=_VIS)
     @test length(p) == 485
     @test length(t) == 2207
+    p,t = distmesh(d,huniform,0.2, vis=_VIS, distribution=:packed)
+    @test length(p) == 742
+    @test length(t) == 3455
 end
