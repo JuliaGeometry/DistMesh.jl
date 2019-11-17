d90756218b9c8412ef1bf239d689563eee8c5505
Steve Kelly <kd2cca@gmail.com>
2019-11-17 12:19:48 -0500

    add profile

---
+++ bench/profile.jl
@@ -0,0 +1,10 @@
+using DistMesh
+using GeometryBasics
+using PProf
+
+fn_sphere(v) = sqrt(sum(v.^2)) -1
+algo = DistMeshSetup(distribution=:packed)
+
+distmesh(fn_sphere,huniform,0.15,algo,origin = GeometryBasics.Point{3,Float64}(-1),widths = GeometryBasics.Point{3,Float64}(2),stats=false)
+
+@pprof distmesh(fn_sphere,huniform,0.15,algo,origin = GeometryBasics.Point{3,Float64}(-1),widths = GeometryBasics.Point{3,Float64}(2),stats=false)
