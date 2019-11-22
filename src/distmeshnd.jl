5d38ef93820e2c1098d32c63fdbd7e056afae0ef
Steve Kelly <kd2cca@gmail.com>
2019-11-22 18:26:22 -0500

    factor out filtering delaunay triangulation

--- src/distmeshnd.jl
+++ src/distmeshnd.jl
@@ -104,24 +104,8 @@ function distmesh(fdist::Function,
 
         # if large move, retriangulation
         if maxmove>setup.ttol*h
-            triangulation = delaunayn(p)
-            t_d = triangulation.tetrahedra
-            resize!(t, length(t_d))
-            copyto!(t, t_d) # we need to copy since we have a shared reference with tetgen
-
-            # average points to get mid point of each tetrahedra
-            # if the mid point of the tetrahedra is outside of
-            # the boundary we remove it.
-            # TODO: this is an inlined filter call. Would be good to revert
-            # TODO: can we use the point distance array to pass boundary points to
-            #        tetgen so this call is no longer required?
-            j = firstindex(t)
-            for ai in t
-                t[j] = ai
-                pm = (p[ai[1]].+p[ai[2]].+p[ai[3]].+p[ai[4]])./4
-                j = ifelse(fdist(pm) <= -geps, nextind(t, j), j)
-            end
-            j <= lastindex(t) && resize!(t, j-1)
+            # compute a new delaunay triangulation
+            delaunayn!(fdist, p, t, geps)
 
             # 4. Describe each edge by a unique pair of nodes
             tet_to_edges!(pair, pair_set, t)
