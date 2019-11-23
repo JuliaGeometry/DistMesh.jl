ebf7e7eefbd0863f5c447f1e3455b7da74ebfb20
Steve Kelly <kd2cca@gmail.com>
2019-11-22 21:27:43 -0500

    cleanup comments

--- src/distmeshnd.jl
+++ src/distmeshnd.jl
@@ -23,6 +23,7 @@ function distmesh(fdist::Function,
                   widths=GeometryBasics.Point{3,Float64}(2,2,2),
                   fix=nothing,
                   stats=false) where {VertType}
+    # TODO: tetgen only handles Float64
     if isa(fix, Nothing)
         VT = promote_type(typeof(origin), typeof(widths))
         fp = nothing
@@ -45,24 +46,16 @@ function distmesh(fdist::Function,
                   ::Val{stats},
                   ::Type{VertType}) where {VertType, stats}
 
-    geps=1e-1*h+setup.iso
+    geps=1e-1*h+setup.iso # parameter for filtering tets outside bounds and considering for max displacment of a node
 
     # static parameter info
     non_uniform = isa(fh, Function) # so we can elide fh calls
     have_fixed = !isa(fix, Nothing)
 
-    deps = sqrt(eps(eltype(VertType))) # epsilon for computing central difference
     #ptol=.001; ttol=.1; L0mult=1+.4/2^(dim-1); deltat=.2; geps=1e-1*h;
 
-    # # % 2. Remove points outside the region, apply the rejection method
-    # p=p(feval(fdist,p,varargin{:})<geps,:);
-    # r0=feval(fh,p);
-    # p=[fix; p(rand(size(p,1),1)<min(r0)^dim./r0.^dim,:)];
-    # N=size(p,1);
-
     # initialize Vertex Arrays
-    # the first N points in the array correspond to
-    # 'fix' points that do not move
+    # the first N points in the array correspond to'fix' points that do not move
     if have_fixed
         p = copy(fix)
     else
@@ -80,35 +73,31 @@ function distmesh(fdist::Function,
     pt_dists = map(fdist, p) # cache to store point locations so we can minimize fdist calls
 
     # initialize arrays
-    pair_set = Set{Tuple{Int32,Int32}}() # set used for ensure we have a unique set of edges
-    pair = Tuple{Int32,Int32}[] # edge indices (Int32 since we use Tetgen)
-    dp = fill(zero(VertType), length(p)) # force at each node
-    bars = VertType[] # the vector of each edge
-    L = eltype(VertType)[] # vector length of each edge
-    non_uniform && (L0 = eltype(VertType)[]) # desired edge length computed by dh (edge length function)
-    t = GeometryBasics.SimplexFace{4,Int32}[] # tetrahedra indices from delaunay triangulation
-    maxmove = typemax(eltype(VertType)) # stores an iteration max movement for retriangulation
+    pair_set = Set{Tuple{Int32,Int32}}()        # set used for ensure we have a unique set of edges
+    pair = Tuple{Int32,Int32}[]                 # edge indices (Int32 since we use Tetgen)
+    dp = fill(zero(VertType), length(p))        # force at each node
+    bars = VertType[]                           # the vector of each edge
+    L = eltype(VertType)[]                      # vector length of each edge
+    non_uniform && (L0 = eltype(VertType)[])    # desired edge length computed by dh (edge length function)
+    t = GeometryBasics.SimplexFace{4,Int32}[]   # tetrahedra indices from delaunay triangulation
+    maxmove = typemax(eltype(VertType))         # stores an iteration max movement for retriangulation
 
-    # array for tracking quality metrics
-    tris = NTuple{3,Int32}[] # array to store triangles used for quality checks
+    # arrays for tracking quality metrics
+    tris = NTuple{3,Int32}[]        # triangles used for quality checks
     triset = Set{NTuple{3,Int32}}() # set for triangles to ensure uniqueness
     qualities = eltype(VertType)[]
-    #maxmoves = eltype(VertType)[]
 
     # information on each iteration
     statsdata = DistMeshStatistics()
     lcount = 0 # iteration counter
 
     @inbounds while true
-        # Retriangulation by Delaunay
-
         # if large move, retriangulation
         if maxmove>setup.ttol*h
-            # compute a new delaunay triangulation
-            delaunayn!(fdist, p, t, geps)
 
-            # 4. Describe each edge by a unique pair of nodes
-            tet_to_edges!(pair, pair_set, t)
+            delaunayn!(fdist, p, t, geps) # compute a new delaunay triangulation
+
+            tet_to_edges!(pair, pair_set, t) # Describe each edge by a unique pair of nodes
 
             # resize arrays for new pair counts
             resize!(bars, length(pair))
@@ -118,7 +107,8 @@ function distmesh(fdist::Function,
             stats && push!(statsdata.retriangulations, lcount)
         end
 
-        # 6. Move mesh points based on edge lengths L and forces F
+        # compute edge lengths (L) and adaptive edge lengths (L0)
+        # Lp norm (p=3) is partially computed here
         Lsum = zero(eltype(L))
         L0sum = non_uniform ? zero(eltype(L0)) : length(pair)
         for i in eachindex(pair)
@@ -138,11 +128,11 @@ function distmesh(fdist::Function,
             dp[i] = zero(VertType)
         end
 
-        # this is not hoisted correctly in the loop
-        # so we initialize here
-        # this is the Lp norm (p=3)
+        # this is not hoisted correctly in the loop so we initialize here
+        # finish computing the Lp norm (p=3)
         lscbrt = (1+(0.4/2^2))*cbrt(Lsum/L0sum)
 
+        # Move mesh points based on edge lengths L and forces F
         for i in eachindex(pair)
             L0_f = non_uniform ? L0[i].*lscbrt : lscbrt
             # compute force vectors
@@ -174,25 +164,24 @@ function distmesh(fdist::Function,
             p0 = p[i] # store original point location
             p[i] = p[i].+setup.deltat.*dp[i] # apply displacements to points
 
-            # check if we are verifiably within the bounds and use this value
-            # to avoid recomputing fdist
-            move = sqrt(sum((p[i]-p0).^2))
-            d_est = pt_dists[i] + move # apply the movement to our cache point
-            d = d_est < -geps ? d_est : fdist(p[i])
-            pt_dists[i] = d # store the correct or approximate distance from the function
+            # Check if we are verifiably within the bounds and use this value
+            # to avoid recomputing fdist. This increases performance greatly on
+            # complex distance functions and large node counts
+            move = sqrt(sum((p[i]-p0).^2))          # compute movement from the displacement
+            d_est = pt_dists[i] + move              # apply the movement to our cache point
+            d = d_est < -geps ? d_est : fdist(p[i]) # determine if we need correct or approximate distance
+            pt_dists[i] = d                         # store distance
 
             if d < -geps
                 maxdp = max(maxdp, setup.deltat*sqrt(sum(dp[i].^2)))
             end
 
             if d <= setup.iso
-                maxmove = max(move,maxmove) # determine movements
+                maxmove = max(move,maxmove)
                 continue
             end
 
-            # bring points back to boundary if outside
-            #deps = sqrt(eps(d)) # this is quite an expensive call, we use a constant initialized in the beginning
-            # use central difference and project back to boundary
+            # bring points back to boundary if outside using central difference
             p[i] = p[i] .- centraldiff(fdist,p[i]).*(d+setup.iso)
             maxmove = max(sqrt(sum((p[i]-p0).^2)), maxmove)
             pt_dists[i] = setup.iso # ideally
@@ -211,7 +200,7 @@ function distmesh(fdist::Function,
             push!(statsdata.median_qual, qualities[round(Int,length(qualities)/2)])
         end
 
-        # 8. Termination criterion
+        # Termination criterion
         if maxdp<setup.ptol*h
             return p, t, statsdata
         end
