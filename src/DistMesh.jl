33804846cc5b43f63ca846e4a392cf33a880239c
Steve Kelly <kd2cca@gmail.com>
2019-11-23 09:35:27 -0500

    init quality-based meshing algo

--- src/DistMesh.jl
+++ src/DistMesh.jl
@@ -8,6 +8,7 @@ using GeometryBasics: Triangle, Tetrahedron, Mesh, Polytope, Point
 const tetpairs = ((1,2),(1,3),(1,4),(2,3),(2,4),(3,4))
 const tettriangles = ((1,2,3),(1,2,4),(2,3,4),(1,3,4))
 
+abstract type AbstractDistMeshAlgorithm end
 
 """
     DistMeshSetup
@@ -15,7 +16,7 @@ const tettriangles = ((1,2,3),(1,2,4),(2,3,4),(1,3,4))
     iso (default: 0): Value of which to extract the iso surface, inside negative
     deltat (default: 0.1): the fraction of edge displacement to apply each iteration
 """
-struct DistMeshSetup{T}
+struct DistMeshSetup{T} <: AbstractDistMeshAlgorithm
     iso::T
     deltat::T
     ttol::T
@@ -30,6 +31,32 @@ function DistMeshSetup(;iso=0,
                         distribution=:regular)
     T = promote_type(typeof(iso),typeof(ptol),typeof(deltat), typeof(ttol))
     DistMeshSetup{T}(iso,
+                     deltat,
+                     ttol,
+                     ptol,
+                     distribution)
+end
+
+"""
+    DistMeshQuality
+
+    iso (default: 0): Value of which to extract the iso surface, inside negative
+    deltat (default: 0.1): the fraction of edge displacement to apply each iteration
+"""
+struct DistMeshQuality{T} <: AbstractDistMeshAlgorithm
+    iso::T
+    deltat::T
+    minimum::T
+    distribution::Symbol # intial point distribution
+end
+
+function DistMeshQuality(;iso=0,
+                        ptol=.001,
+                        deltat=0.05,
+                        ttol=0.02,
+                        distribution=:regular)
+    T = promote_type(typeof(iso),typeof(ptol),typeof(deltat), typeof(ttol))
+    DistMeshQuality{T}(iso,
                      deltat,
                      ttol,
                      ptol,
