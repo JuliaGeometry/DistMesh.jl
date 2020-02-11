6342a602b481435f25975d67af32a74376d300a3
Steve Kelly <kd2cca@gmail.com>
2020-02-11 16:49:36 -0500

    cleanup plot outputs

--- bench/util.jl
+++ bench/util.jl
@@ -13,14 +13,9 @@ function plotout(statsdata, qualities, folder, name)
 
     # max_plt = Plots.plot(statsdata.maximum_qual, title = "Maximum Tri Quality", legend=false, ylabel="Quality")
     # vline!(statsdata.retriangulations, line=(0.2, :dot, [:red]))
+    data = hcat(statsdata.average_volume_edge_ratio, statsdata.min_volume_edge_ratio, statsdata.max_volume_edge_ratio)
 
-    avg_tq_plt = Plots.plot(statsdata.average_volume_edge_ratio, title = "Average Tet Quality", legend=false, ylabel="Vol/Edge Ratio")
-    vline!(statsdata.retriangulations, line=(0.2, :dot, [:red]))
-
-    min_tq_plt = Plots.plot(statsdata.min_volume_edge_ratio, title = "Minimum Tet Quality", legend=false, ylabel="Vol/Edge Ratio")
-    vline!(statsdata.retriangulations, line=(0.2, :dot, [:red]))
-
-    max_tq_plt = Plots.plot(statsdata.max_volume_edge_ratio, title = "Maximum Tet Quality", legend=false, ylabel="Vol/Edge Ratio")
+    tq_plt = Plots.plot(data, title = "Tet Quality", legend=true, label=["Avg","Min","Max"], ylabel="Vol/Edge Ratio")
     vline!(statsdata.retriangulations, line=(0.2, :dot, [:red]))
 
     maxdp_plt = Plots.plot(statsdata.maxdp, title = "Max Displacement", legend=false, ylabel="Edge Displacement")
@@ -29,7 +24,7 @@ function plotout(statsdata, qualities, folder, name)
     maxmove_plt = Plots.plot(statsdata.maxmove, title = "Max Move", legend=false, ylabel="Point Displacement")
     vline!(statsdata.retriangulations, line=(0.2, :dot, [:red]))
 
-    plt = Plots.plot(avg_tq_plt,min_tq_plt,max_tq_plt,maxdp_plt,maxmove_plt,layout=(5,1), xlabel="Iteration")
+    plt = Plots.plot(tq_plt,maxdp_plt,maxmove_plt,layout=(3,1), xlabel="Iteration")
 
     savefig(plt, "$folder/result_stat$name.svg")
     savefig(qual_hist, "$folder/result_qual$name.svg")
