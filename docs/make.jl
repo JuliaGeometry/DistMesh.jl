0d91109e33780e3367b22ff6bf9185cc09b0deed
Viral B. Shah <viral@mayin.org>
2021-10-10 12:03:15 -0400

    documenter make.jl updates

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,16 @@
 using Documenter, DistMesh
 
-makedocs(;
+makedocs(
     modules=[DistMesh],
     format=Documenter.HTML(),
     pages=[
         "Home" => "index.md",
     ],
-    repo="https://github.com/juliageometry/DistMesh.jl/blob/{commit}{path}#L{line}",
     sitename="DistMesh.jl",
     authors="Steve Kelly <kd2cca@gmail.com>",
     assets=String[],
 )
 
-deploydocs(;
-    repo="github.com/juliageometry/DistMesh.jl",
+deploydocs(
+    repo="github.com/JuliaGeometry/DistMesh.jl",
 )
